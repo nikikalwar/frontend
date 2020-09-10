8c24c0537a96a96b1cfb5fcbba86d3336d28bdb6
nikikalwar <nikikumar27@gmail.com>
2020-09-11 01:48:22 +0530

    Images list in html

---
+++ es6/song.js
@@ -0,0 +1,19 @@
+//homework assignment
+
+var genre="Jazz";
+var artist="Niki";
+var year="2020";
+var durationInSeconds=328;
+var country="Nepal";
+var popularity="high";
+
+console.log(artist);//artist name
+console.log(genre);//song genre
+console.log(durationInSeconds);//durationInSeconds
+console.log(year);//year the song was published
+console.log(country);//country the song was published 
+console.log(popularity);//popularity of the song
+
+
+
+
