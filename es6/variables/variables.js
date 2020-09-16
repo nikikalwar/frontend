ab952892a83a386d94c27e4a0d5b88a1da279e9b
nikikalwar <nikikumar27@gmail.com>
2020-09-16 22:27:46 +0530

    refreshing the var keyword

---
+++ es6/variables/variables.js
@@ -0,0 +1,32 @@
+//Use of var keyword
+/*Remove this line to view the usage of var keyword
+
+function newName(){
+    var myName="Niki";
+    if(typeof myName==="string"){
+        console.log(myName);
+    }
+    
+}
+//eg1
+newName();
+console.log(myName);//this will give an error "myName is not defined"
+
+//apparently in ES6 this will go through without an error and the value will be modified even its declared inside
+//a function 
+myName="Chris";
+console.log(myName);
+
+*/
+
+//so here we are dealing with variable scope problems
+
+
+
+
+
+
+
+
+
+
