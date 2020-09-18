39125da49474c5114a1bbd44653c000a4be7b258
nikikalwar <nikikumar27@gmail.com>
2020-09-18 15:51:42 +0530

    more examples in logical operators

--- es6/Conditional/logical.js
+++ es6/Conditional/logical.js
@@ -4,7 +4,25 @@
 const bankAccountBalance=1234;
 const cost=123;
 const canSpend=true;
+const hasCreditCard=true;
+const hasReachedLimit=false;
 
+if(hasReachedLimit!==true){
+    console.log("you can still make the purchase");//prints
+}
+
+if(!hasReachedLimit){
+    console.log("you can make the purchase");//prints
+}
+
+let myName;
+console.log(myName); //prints undefined
+
+console.log(!myName);//prints true
+
+
+
+/*
 if(bankAccountBalance>=cost && cost<150)
 {
     if(canSpend===true)
@@ -12,3 +30,4 @@ if(bankAccountBalance>=cost && cost<150)
 }
 //using nested if statements //can be done in single line
 
+*/
