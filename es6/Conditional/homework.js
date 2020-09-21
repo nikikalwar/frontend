3c4ec9b0b91c3f4bc6751ca47302b1d8b4a8c37c
nikikalwar <nikikumar27@gmail.com>
2020-09-21 15:01:03 +0530

    Syllogism example

---
+++ es6/Conditional/homework.js
@@ -0,0 +1,20 @@
+/* All men are mortal 
+    Socrates is a man
+    Therefore,socrates is mortal. */
+
+  //understanding Syllogism
+  //categorical Syllogism
+
+  //if A is part of C, then B is part of C" logic
+
+const mortal="men";
+const socrates="man";
+
+if(socrates==="man" && mortal==="men"){
+    console.log("Therefore,socrates is mortal");
+}
+
+
+
+   
+    
