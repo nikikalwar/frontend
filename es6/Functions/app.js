0a5c91bbd89c942d4cb8ee1b01b1d53d7f08ae97
nikikalwar <nikikumar27@gmail.com>
2020-09-23 17:05:04 +0530

    passing values/no values and default values for function

--- es6/Functions/app.js
+++ es6/Functions/app.js
@@ -108,13 +108,13 @@ function squareAndMultiply(num1,num2){
 console.log(squareAndMultiply(2,4));
 console.log(squareAndMultiply(6,6));
 */
-
+/*
 function nameFormatter(firstName,lastName,age){
     function formatNames(arg1,arg2){
             return arg1+" "+arg2;
     }
 
-    
+    //formats data into an object
     function dataFormatter(fullname,num){
         const formattedData={
             name:fullname,
@@ -124,12 +124,73 @@ function nameFormatter(firstName,lastName,age){
     }
     const formattedName=formatNames(firstName,lastName);
     return dataFormatter(formattedName,age);
-
 }
 
 console.log(nameFormatter("chris","Jones",20));
+    */
+
+    //Advance functions in Javascript
+        //arrow function in Javascript
+/*
+        var sayHi=()=>{
+            console.log("Say Hi");
+        }
+
+        sayHi();
+    
+        const multiplier=(num1,num2)=>{
+            return num1*num2;
+        }
+
+        console.log(multiplier(5,10));
+*/
+/*
+const users=[
+    {name:"Chris", age:60},
+    {name:"Jane", age:30},
+    {name:"Billy",age:45}
+];
+
+const userName=users.map(function(user){
+    return user.name;
+});
+console.log(userName);//[ 'Chris', 'Jane', 'Billy' ]
+*/
+//another method to use arrow functions
+//if single statement is there we can omit return keyword
+/*
+const squarer=num => num*num;
+console.log(squarer(5));
+*/
+/*
+function numberMultiplier(x,y){
+    x=x||5;//picks the default value 3
+    y=y||10;//picks the value 10
+    return x*y;
+}
+console.log(numberMultiplier(3));//outputs 30
+*/
+/*
+const num=(x=10,y=2)=>{
+    x=x||5;
+    y=y||3;
+    return x*y;
+}
+
+console.log(num(2,3));//6
+console.log(num(2));//6
+console.log(num(2,3));
+console.log(num());//if no parameters are passed it picks up default values
+*/
+/*
+const intialString="Hi How are you".split(" ");
+console.log(intialString);
+
+*/
 
 
+
+ 
 
 
 
