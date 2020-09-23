7bf9454d30f15a1914ae4883e51360d01d8239bd
nikikalwar <nikikumar27@gmail.com>
2020-09-23 14:50:23 +0530

    more examples in function

--- es6/Functions/app.js
+++ es6/Functions/app.js
@@ -39,7 +39,7 @@ const myName="Niki";//due to hoisting the variables are initialized with undefin
 sayHi();
 
 */
-
+/*
 var squaredNumber = function(num) {
     return num*num;
 }
@@ -49,6 +49,54 @@ var numberAdder=function(num1,num2){
 }
 
 console.log(numberAdder(squaredNumber(5),10)); //outputs 35
+
+*/
+/*
+let myNumber=100;
+
+function addTwenty(num){
+    return num+20;
+}
+console.log(addTwenty(myNumber));//returns the added number
+console.log(myNumber);//returns 100--the number is not modified
+
+let myString="Niki";
+
+function changeString(anything){
+    return "Hello "+anything; 
+}
+console.log(changeString(myString));//return Hello Niki
+console.log(myString); //returns Niki-- the original string is not modified
+
+*/
+/*
+const myInfo={name:"Chris", age:30};
+
+function changeAge(myObj){
+    myObj.age=100;
+}
+
+changeAge(myInfo);
+
+console.log(myInfo);//age is modified to 100-->with objects we can mutuate the properties value
+*/
+/*
+const myNumbers=[1,2,3,4,5];
+
+function addToArray(myarr){
+    return myarr.push(6);
+}
+
+console.log(myNumbers);///outputs original array
+addToArray(myNumbers);
+console.log(myNumbers);//outputs changed array
+
+*/
+
+
+
+
+
 
 
 
