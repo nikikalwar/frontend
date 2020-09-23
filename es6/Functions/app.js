904e7bcc7277ed51a2a51d1b3df1293c117b1e36
nikikalwar <nikikumar27@gmail.com>
2020-09-23 13:47:04 +0530

    simple example of functions passing parameters and reusable functions

---
+++ es6/Functions/app.js
@@ -0,0 +1,15 @@
+//functions
+
+/*
+"A function is a javascript procedure - a set of statements that performs a task or calculates a value.[...]";
+--Mozilla develeoper network
+*/
+
+
+function sayHi(userName){
+    console.log("Hi "+userName);
+
+}
+
+sayHi("Niki");
+sayHi("Billy");
