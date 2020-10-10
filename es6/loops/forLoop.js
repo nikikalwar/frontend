c361d1c6e8022a5bfdcee7ddfbf86a1f1f525fd9
nikikalwar <nikikumar27@gmail.com>
2020-10-11 03:54:07 +0530

    different way of using break statements

--- es6/loops/forLoop.js
+++ es6/loops/forLoop.js
@@ -7,7 +7,7 @@ for(index;index<10;index++){
     console.log(index);
 }
 */
-
+/*
 for(let i=1;i<=100;i+=1){
     if(i%3===0 && i%5===0) console.log("FizzBuzz");
     else if(i%3===0){
@@ -21,3 +21,34 @@ for(let i=1;i<=100;i+=1){
         console.log(i);
     }
 }
+
+*/
+/* eg of continue
+let x=0;
+while(x<10){
+    x+=1;
+    if(x===5){
+        console.log("I am skipped");
+        continue;
+    }
+    console.log(x);
+}
+
+*/
+/* demonstration of loops
+*/
+/*
+outerLoop:
+for(let i=0;i<10;i++){
+    console.log("loop# "+i);
+    innerLoop:
+    for(let a=0;a<10;a++){
+        console.log("looping inner "+a);
+      //  if(a==3) break outerLoop;
+      if(a===3) break innerLoop;
+    }
+    if(i===5) break outerLoop;
+}
+*/
+
+
