d624116780954a420d6130078464cc5912406566
nikikalwar <nikikumar27@gmail.com>
2020-10-13 04:28:02 +0530

    checking out crypto-js

---
+++ es6/DOM/misc/app.js
@@ -0,0 +1,10 @@
+var AES = require("crypto-js/aes");
+var SHA256 = require("crypto-js/sha256");
+var hash = CryptoJS.SHA1("Message");
+console.log(hash)
+
+
+
+var CryptoJS = require("crypto-js");
+//console.log(CryptoJS.HmacSHA1("Message", "Key"));
+
