afff24ebdd03f57a36eb03f2f52985abc6f49559
nikikalwar <nikikumar27@gmail.com>
2020-10-13 21:27:33 +0530

    issue faced:bubbling up of events in js

--- es6/events/app.js
+++ es6/events/app.js
@@ -7,6 +7,7 @@ can be user-generated or by an API
 
 
 */
+const greenBackground=document.querySelector("#list-items");
 const clickableButton=document.querySelector("button");
 const clickDiv=document.querySelector("#clicky");
 /*
@@ -29,6 +30,32 @@ function logEvent(e){
         console.log(e);
         clickableButton.removeEventListener("click",logEvent);
     }
-   //removing the eventListener so that the button is unclick
-   //able after a single click
+    //removing the eventListener so that the button is unclick
+    //able after a single click
 }
+
+
+
+    const background=document.querySelector("#background");
+
+  background.addEventListener("click",switchBackground);  
+function switchBackground(e) {
+    if(e.type==="click"){
+        background.classList.add("background-color");
+        console.log(background);
+
+    }
+    background.removeEventListener("click",switchBackground)
+}
+greenBackground.addEventListener("click",green);  
+function green(e) {
+   // e.stopPropagation();
+    if(e.type==="click"){
+        greenBackground.classList.add("green-background");
+        console.log(background);
+        greenBackground.removeEventListener("click",green);
+    }
+
+}
+
+
