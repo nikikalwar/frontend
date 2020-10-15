a5029e4e88dc86ee0b794a5ef353ce651c0d1d77
nikikalwar <nikikumar27@gmail.com>
2020-10-15 08:34:59 +0530

    added basic styling

--- es6/tic_tac_toe/app.js
+++ es6/tic_tac_toe/app.js
@@ -12,26 +12,28 @@ for(let i=0;i<9;i++){
 let str=[];
 function markXY(e){
     count++;
-    console.log(e.target.id[e.target.id.length-1]);
-   
+   // console.log(e.target.id[e.target.id.length-1]);
+
     e.stopPropagation();
     const target=document.querySelector("#"+e.target.id);
-    console.log(target);
+   // console.log(target);
     const h1=document.createElement("h1");
     h1.classList="h1-class";
-    console.log(h1);
-   target.appendChild(h1);
+  //  console.log(h1);
+   
     if(count%2==0)
     {  str[e.target.id[e.target.id.length-1]]="O";
     h1.textContent="O";
+    h1.style.color="black";
 }
     else {
         str[e.target.id[e.target.id.length-1]]="X";
         h1.innerHTML="X";
+        h1.style.color="red";
     }
    
    // str+=h1.innerHTML;
-  
+   target.appendChild(h1);
    if(count>=3)
             winner();    
  
@@ -52,9 +54,9 @@ function winner(){
     for(let i=1;i<=count;i=i+3){
          // str1=str[i];    
         // console.log(i);
-      if(str[i]===str[i+1]&&str[i+1]===str[i+2] &&typeof str[i]!=='undefined'){
-              win_1.textContent=`Player ${str[i]} has won`;
-              console.log("this part is having issue");
+      if(str[i]===str[i+1]&&str[i]===str[i+2] &&typeof str[i]!=='undefined'){
+            setTimeout( alert(win_1.textContent=`Player ${str[i]} has won`),1000);
+            //  console.log("this part is having issue");
                 gameOver();
              }
          }
@@ -63,7 +65,8 @@ function winner(){
          
         for(let i=1;i<=3;i++){
             if(str[i]===str[i+3]&&str[i]===str[i+6] && typeof str[i]==="string"){
-               win_1.textContent=`Player ${str[i]} has won`;
+                setTimeout( alert(win_1.textContent=`Player ${str[i]} has won`),1000);
+
              //  console.log("this part is having issue");
                gameOver();
             }
@@ -72,17 +75,17 @@ function winner(){
         //3rd part for all diagonal elements
         if(str[1]===str[5] && str[5]===str[9] && typeof str[1]==="string")
         {
-           win_1.textContent=`Player ${str[1]} has won`;
+           setTimeout( alert(  win_1.textContent=`Player ${str[1]} has won`),1000);
            gameOver();
         }
          if(str[3]===str[5] && str[5]===str[7] && typeof str[3]==="string" )
         {
-           win_1.textContent=`Player ${str[3]} has won`;
+         setTimeout(  alert(win_1.textContent=`Player ${str[3]} has won`),1000);
            gameOver();
         } 
         
-        else if(win_1.textContent==="Game is on" &&count>9){
-            win_1.textContent="Nobody won ,let's try again";
+        else if(win_1.textContent==="Game is on" && count>=9){
+          setTimeout(alert(  win_1.textContent="Nobody won ,let's try again"));
             gameOver();
         }
        
@@ -90,9 +93,9 @@ function winner(){
 }
 
 function gameOver(){
-    setTimeout(function(){ alert("Game over"); }, 2000);
+    setTimeout(function(){  location.reload();}, 2000);
     console.log("Game over");
-   // location.reload();
+   
 }
 
 
