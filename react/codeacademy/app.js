cae1eb76193e39b158ae3cd5f662934ee3043619
nikikalwar <nikikumar27@gmail.com>
2020-10-17 00:58:14 +0530

    basic JSX

---
+++ react/codeacademy/app.js
@@ -0,0 +1,87 @@
+/*
+
+const myArticle=<article>THis is a article</article>
+
+
+const blog = (
+
+<div>
+  <img src="pics/192940u73.jpg" />
+  <h1>
+    Welcome to Dan's Blog!
+  </h1>
+  <article>
+    Wow I had the tastiest sandwich today.  I <strong>literally</strong> almost freaked out.
+  </article>
+</div>
+);
+   */
+
+   /*
+
+   ///basic hello world rendering
+
+   import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Copy code here:
+ReactDOM.render(<h1>Hello world</h1>, document.getElementById('app'));
+
+*/
+
+/*ReactDOM.render()
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+ReactDOM.render(<h1>Render me!</h1>,document.getElementById('app'))
+
+*/
+
+/*2nd
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+ReactDOM.render(<h1>Render me!</h1>, document.getElementById('container'));
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="/styles.css">
+	<title>Learn ReactJS</title>
+</head>
+
+<body>
+<span id="container"></span>
+	<script src="https://content.codecademy.com/courses/React/react-course-bundle.min.js"></script>
+  <script src="/app.compiled.js"></script>
+</body>
+
+</html>
+*/
+
+/*
+
+//Passing a Variable to ReactDOM.render()
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+const myList=(
+  <ul>
+      <li>JSX elements are the unit of JSX expression</li>
+      <li>Just combined html and JS</li>
+       <li>It's easy to understand if you are good with JS</li>
+        <li>Let's see the next concepts</li>
+         <li>That's all</li>
+  </ul>
+);
+
+ReactDOM.render(myList,document.getElementById('app'));
+
+*/
