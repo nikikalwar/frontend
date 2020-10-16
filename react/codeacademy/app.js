1e84b9717fa618fabbd683ae6546161989a70a28
nikikalwar <nikikumar27@gmail.com>
2020-10-17 05:09:27 +0530

    done with advanced JSX

--- react/codeacademy/app.js
+++ react/codeacademy/app.js
@@ -153,3 +153,166 @@ ReactDOM.render(gooseImg,document.getElementById('app'));
 
 */
 
+/*event listeners
+
+Event Listeners in JSX
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function makeDoggy(e) {
+  // Call this extremely useful function on an <img>.
+  // The <img> will become a picture of a doggy.
+  e.target.setAttribute('src', 'https://content.codecademy.com/courses/React/react_photo-puppy.jpeg');
+  e.target.setAttribute('alt', 'doggy');
+}
+
+const kitty = (
+	<img 
+		src="https://content.codecademy.com/courses/React/react_photo-kitty.jpg" 
+		alt="kitty" 
+    onClick={makeDoggy}
+    />
+);
+
+ReactDOM.render(kitty, document.getElementById('app'));
+
+*/
+
+/* using if else in JSX
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function coinToss() {
+  // This function will randomly return either 'heads' or 'tails'.
+  return Math.random() < 0.5 ? 'heads' : 'tails';
+}
+
+const pics = {
+  kitty: 'https://content.codecademy.com/courses/React/react_photo-kitty.jpg',
+  doggy: 'https://content.codecademy.com/courses/React/react_photo-puppy.jpeg'
+};
+let img;
+
+
+
+// if/else statement begins here:
+if (coinToss() === "heads"){
+img=  <img src={pics.kitty} />
+}
+
+else{
+ img= <img src={pics.doggy} />
+}
+
+ReactDOM.render(img.document.getElementById('app'));
+
+*/
+/* ternary operators in JSX
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function coinToss () {
+  // Randomly return either 'heads' or 'tails'.
+  return Math.random() < 0.5 ? 'heads' : 'tails';
+}
+
+const pics = {
+  kitty: 'https://content.codecademy.com/courses/React/react_photo-kitty.jpg',
+  doggy: 'https://content.codecademy.com/courses/React/react_photo-puppy.jpeg'
+};
+
+const img = <img src={pics[coinToss()==="heads"?"kitty":"doggy"]} />;
+
+ReactDOM.render(
+	img, 
+	document.getElementById('app')
+);
+
+*/
+
+/* &&operator in JSX
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// judgmental will be true half the time.
+const judgmental = Math.random() < 0.5;
+
+const favoriteFoods = (
+  <div>
+    <h1>My Favorite Foods</h1>
+    <ul>
+      <li>Sushi Burrito</li>
+      <li>Rhubarb Pie</li>
+    {!judgmental&&<li>Nacho Cheez Straight Out The Jar</li>
+      }  <li>Broiled Grapefruit</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(
+
+	favoriteFoods, 
+	document.getElementById('app')
+);
+
+*/
+
+/*.map function
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const people = ['Rowe', 'Prevost', 'Gare'];
+
+const peopleLis = people.map(person =>
+  // expression goes here:
+  <li>{person}</li>
+);
+
+// ReactDOM.render goes here:
+
+ReactDOM.render(<ul>{peopleLis}</ul>,
+  document.getElementById('app')
+);
+*/
+
+/*Keys in JSX
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const people = ['Rowe', 'Prevost', 'Gare'];
+
+
+const peopleLis = people.map((person,i) =>
+  // expression goes here:
+  <li key={"person_" + i}>{person}</li>
+);
+
+// ReactDOM.render goes here:
+
+ReactDOM.render(<ul>{peopleLis}</ul>,
+  document.getElementById('app')
+);
+
+*/
+
+/*
+React create element
+
+const greatestDivEver = React.createElement(
+  "div",
+null,
+"i am div"
+);
+
+*/
+
+
+
+
+
