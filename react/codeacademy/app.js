63d621952332ebd90b6e6fcafdd7a733d996e1ac
nikikalwar <nikikumar27@gmail.com>
2020-10-22 02:43:39 +0530

    advanced JSX and components/this

--- react/codeacademy/app.js
+++ react/codeacademy/app.js
@@ -316,3 +316,45 @@ null,
 
 
 
+/*
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const fiftyFifty = Math.random() < 0.5;
+
+// New component class starts here:
+class TonightsPlan extends React.Component{
+  render(){
+    if(fiftyFifty)
+    return <h1>Tonight I'm going out WOOO</h1>;
+    else 
+    return <h1>Tonight I'm going to bed WOOO</h1>;
+  }
+  
+}
+
+ReactDOM.render(<TonightsPlan />,document.getElementById('app'));
+
+*/
+
+/* use this 
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MyName extends React.Component {
+	// name property goes here:
+get name() {
+  return 'Niki';
+}
+
+  render() {
+    return <h1>My name is {this.name}.</h1>;
+  }
+}
+
+ReactDOM.render(<MyName />, document.getElementById('app'));
+
+*/
