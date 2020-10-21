fef91011658a4e105f450fb962d18dc822bebba4
nikikalwar <nikikumar27@gmail.com>
2020-10-21 14:45:19 +0530

    list.html is able to generate the page dynamically from the localStorage

--- todo/list.js
+++ todo/list.js
@@ -39,6 +39,113 @@ function todo(){
 
    console.log(g);
 
+   e=localStorage.getItem(localStorage.mySession);
+   console.log(e);
+
+  i=1;
+e=JSON.parse(e); 
+
+for(let prop in e){
+  testprop="tasktime";
+  testTask="taskName";
+  testTask=testTask+i;
+testprop=testprop+i; 
+//console.log(testprop);
+//this one checks for the name of the task
+if(testprop===prop){
+    
+  new_task= document.createElement("div"); 
+  new_task.classList="row";
+  new_task.style="background-color: rgb(212, 212, 212) ;border:2px solid black;margin:2px;padding:2px"
+  
+      new_column=document.createElement("div");
+      new_column.classList="column";
+      new_h2=document.createElement("h2");
+      new_h2.innerHTML=(new Date()).toString().slice(4,25);//e[prop]-- for later
+      new_column.append(new_h2);
+      new_task.append(new_column);
+      document.body.append(new_task);
+
+                    //for the taskname
+              new_column=document.createElement("div");
+              new_column.classList="column";
+              new_h2=document.createElement("h2");
+            
+                new_h2.innerHTML=e["taskName"+i];
+              new_column.append(new_h2);
+              new_task.append(new_column);
+                document.body.append(new_task);
+      //for the checkbox
+
+      
+            new_column=document.createElement("div");
+              new_column.classList="column";
+              new_column.style="text-align: right";
+              new_h2=document.createElement("input");
+            new_h2.key="taskName"+"Input"+"";
+              new_h2.type="checkbox";
+             new_h2.style="width:75%;height:75%;  margin:10px 10px 10px 10px;text-align: right; "> 
+   
+              new_column.append(new_h2);
+              new_task.append(new_column);
+              document.body.append(new_task);
+
+      
+    console.log(e["taskName"+i])
+console.log(prop);
+console.log(e[prop]);
+i++;
+}
+}/*
+i=1;
+for(let prop in e){
+  testprop="taskName";
+testprop=testprop+i; 
+//console.log(testprop);
+//this one checks for the name of the task
+if(testprop===prop){
+    
+     
+      new_column=document.createElement("div");
+      new_column.classList="column";
+      new_h2=document.createElement("div");
+      new_h2.innerHTML=e[prop];
+      new_column.append(new_h2);
+      new_task.append(new_column);
+      document.body.append(new_task);
+      
+    
+console.log(prop);
+console.log(e[prop]);
+i++;
+}
+} 
+/*
+for(let prop in e){
+  testprop="tasktime";
+testprop=testprop+i; 
+//console.log(testprop);
+//this one checks for the name of the task
+if(testprop===prop){
+    
+      new_task= document.createElement("div"); 
+      new_task.classList="row";
+      new_task.style="background-color: rgb(212, 212, 212) ;border:2px solid black;"
+      new_column=document.createElement("div");
+      new_column.classList="column";
+      new_h2=document.createElement("div");
+      new_h2.innerHTML=e[prop];
+      new_column.append(new_h2);
+      new_task.append(new_column);
+      document.body.append(new_task);
+      
+    
+console.log(prop);
+console.log(e[prop]);
+i++;
+}
+}
+*/
    
 
     
